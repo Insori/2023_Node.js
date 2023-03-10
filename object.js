f2c1e23afb0c36dfb7920163136806cadb796c2e
Insori <s110@e-mirim.hs.kr>
2023-03-10 10:02:17 +0900

    object

---
+++ object.js
@@ -0,0 +1,13 @@
+// property(key) : value
+const cathy = {
+    "name" : "cathy",
+    "age" : 19,
+    "skills" : ["자바스크립트", "파이썬", "코볼"]
+};
+
+//{ name: 'cathy', age: 19, skills: ['자바스크립트', '파이썬', '코볼'] }
+console.log(cathy);
+//cathy
+console.log(cathy.name);
+console.log(cathy.age);
+console.log(cathy["name"]);
